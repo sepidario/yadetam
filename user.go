90ca11ee043d5b70c40a979e2bc3b1d6f14c5d19
mehdy <mehdy.khoshnoody@gmail.com>
2018-07-16 00:13:55 +0430

    Add user domain definition

---
+++ user.go
@@ -0,0 +1,15 @@
+package yadetam
+
+// User defines a user
+type User struct {
+	ID   string
+	OS   string
+	Name string
+}
+
+// UserService is the interface that intracts with a user
+type UserService interface {
+	CreateUser(user *User) error
+	UpdateUser(user *User) error
+	DeleteUser(id string) error
+}

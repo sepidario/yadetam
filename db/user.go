701a6de1fa4e41f7f32e902b268641382d352ce6
mehdy <mehdy.khoshnoody@gmail.com>
2018-07-16 10:43:47 +0430

    Add db package

---
+++ db/user.go
@@ -0,0 +1,26 @@
+package db
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/sepidario/yadetam"
+)
+
+// UserService implements yadetam.UserService interface
+type UserService struct {
+	DB *gorm.DB
+}
+
+// CreateUser creates a user
+func (s *UserService) CreateUser(user *yadetam.User) error {
+	panic("not implemented")
+}
+
+// UpdateUser updates a user information (currently only Name)
+func (s *UserService) UpdateUser(user *yadetam.User) error {
+	panic("not implemented")
+}
+
+// DeleteUser deletes a user
+func (s *UserService) DeleteUser(id string) error {
+	panic("not implemented")
+}

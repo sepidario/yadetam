f1eae1183f1104aa41d2e1734564161a52636be0
Mehdy Khoshnoody <mehdy.khoshnoody@gmail.com>
2018-07-16 23:12:08 +0430

    Add basic implementation of UserService

--- db/user.go
+++ db/user.go
@@ -7,20 +7,25 @@ import (
 
 // UserService implements yadetam.UserService interface
 type UserService struct {
-	DB *gorm.DB
+	db *gorm.DB
+}
+
+// NewUserService creates a new user service
+func NewUserService(db *gorm.DB) *UserService {
+	return &UserService{db}
 }
 
 // CreateUser creates a user
 func (s *UserService) CreateUser(user *yadetam.User) error {
-	panic("not implemented")
+	return s.db.Create(user).Error
 }
 
 // UpdateUser updates a user information (currently only Name)
 func (s *UserService) UpdateUser(user *yadetam.User) error {
-	panic("not implemented")
+	return s.db.Save(user).Error
 }
 
 // DeleteUser deletes a user
 func (s *UserService) DeleteUser(id string) error {
-	panic("not implemented")
+	return s.db.Delete(&yadetam.User{ID: id}).Error
 }
